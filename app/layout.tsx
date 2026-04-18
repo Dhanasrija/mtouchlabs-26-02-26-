8a0fed77825e7955458b1569c7b254f4346d1c4e
Srija Aiethamsetti <srijaaiethamsetti@Srijas-MacBook-Air.local>
2026-04-18 09:15:32 +0530

    Latest updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -341,28 +341,30 @@ export default function RootLayout({
           })();
         `}} />
 
-        <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      var Tawk_API = Tawk_API || {};
-      Tawk_API.customStyle = {
-        visibility: {
-          desktop: { position: 'br', xOffset: 20, yOffset: 20 },
-          mobile: { position: 'br', xOffset: 10, yOffset: 70 }
-        }
-      };
-      var Tawk_LoadStart = new Date();
-      (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/614aeb4425797d7a890039a2/1fg6ae1bv';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-      })();
-    `,
-  }}
-/>
+        <Script
+          id="tawk-loader"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {};
+              Tawk_API.customStyle = {
+                visibility: {
+                  desktop: { position: 'br', xOffset: 20, yOffset: 20 },
+                  mobile: { position: 'br', xOffset: 10, yOffset: 70 }
+                }
+              };
+              var Tawk_LoadStart = new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/614aeb4425797d7a890039a2/1fg6ae1bv';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
 
 
       </body>
